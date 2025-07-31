d16eda8fb5ce1629092921f64201e95d03d3b237
Clayton <53104388+claytonfuselier@users.noreply.github.com>
2025-07-31 18:57:15 -0500

    Update index.js

--- naas-api/index.js
+++ naas-api/index.js
@@ -1,28 +1,52 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const fs = require('fs');
+const paht = require('path');
+
+// Get configuration and exit if non-existent
+const configPath = path.resolve(__dirname, '../config/config.json');
+if (!fs.existsSync(configPath)) {
+  console.log('Config file not found at \'${configPath}\'. Exiting...');
+  process.exit(1);
+} else {
+  // Read configuration
+  const config = require(configPath);
+  const endpoint = config.api.endpoint;
+  const rateLimitRequests = config.api?.rateLimit?.requests? || 120; // Fallback to 120
+  const rateLimitWindow = config.api?.rateLimit?.seconds? || 60; // Fallback to 60 seconds
+  const rateLimitOverrides = config.api?.rateLimit?.overrides? || JSON('{}'); // Fallback to an empty object
+  const redirectRoot = config.api?.redirectRoot?.enabled? || true; // Fallback to true
+  const redirectRootDest = config.api.redirectRoot.destination || 'https://github.com/claytonfuselier/no-as-a-service/blob/main/README.md#no-as-a-service'; // Fallback to github repo readme
+}
 
 const app = express();
 app.set('trust proxy', true);
 const PORT = process.env.PORT || 3000; // Fallback to port 3000
 
+// Get IP of caller
+const callerIp = req.headers['cf-connecting-ip'] || req.ip; // Fallback if CF header is missing (or for non-CloudFlare connections)
+
 // Load reasons from JSON
 const reasons = JSON.parse(fs.readFileSync('./reasons.json', 'utf-8'));
 
 // Rate limiter: 120 requests per 60sec per IP
-const RATE_LIMIT_MAX = 120;
-const RATE_LIMIT_WINDOW_SEC = 60;
 const limiter = rateLimit({
-  windowMs: RATE_LIMIT_WINDOW_SEC * 1000, // Convert to ms
-  max: RATE_LIMIT_MAX,
-  keyGenerator: (req, res) => {
-    return req.headers['cf-connecting-ip'] || req.ip; // Fallback if CF header is missing (or for non-CloudFlare connections)
+  windowMs: rateLimitWindow * 1000, // Convert rate limit (secondss) to ms
+  max: (req, res) => {
+    if (rateLimitOverrides[callerIp]) { // Check for custom rate override for the IP
+      return rateLimitOverrides[callerIp]; // Use custom override rate
+    }
+    return config.api.rateLimit.requests; // Use default rate limit
   },
+  keyGenerator: (req, res) => {
+    return callerIp
+  },
+},
   standardHeaders: true, // Includes standardized RateLimit-* headers (RFC-compliant)
   legacyHeaders: false, // Removes outdated headers like X-RateLimit-*
   handler: (req, res) => {
     res.json({
-      error: `Too many requests. (${RATE_LIMIT_MAX}/${RATE_LIMIT_WINDOW_SEC}sec/ip)`
+      error: `Too many requests. (${rateLimitRequests}/${rateLimitWindow}sec/IP)`
     });
   }
 });
@@ -31,13 +55,17 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-// Redirect root path to GitHub
+// Root endpoint
 app.get('/', (req, res) => {
-  res.redirect('https://github.com/claytonfuselier/no-as-a-service/tree/main?tab=readme-ov-file#no-as-a-service');
+  if (redirectRoot) {
+    res.redirect(redirectRootDest); // Redirect root path
+  } else {
+    res.send("No-as-a-Service (NaaS) - created by [claytonfuselier](https://github.com/claytonfuselier/no-as-a-service)");
+  }
 });
 
-// Random rejection reason endpoint
-app.get('/no', (req, res) => {
+// NaaS endpoint
+app.get(endpoint, (req, res) => {
   const reason = reasons[Math.floor(Math.random() * reasons.length)];
   res.json({ reason });
 });
